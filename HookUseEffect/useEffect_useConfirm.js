fec4c528dd587a24abc82b36962732b98930722e
Songpenga <iiuuyy6654@gmail.com>
2023-05-03 22:48:13 +0900

    useConfirm

---
+++ HookUseEffect/useEffect_useConfirm.js
@@ -0,0 +1,30 @@
+import React, {useEffect, useState ,useRef} from "react";
+
+const useConfirm = (message = "", callback, rejection) => {
+  if (typeof callback !== "function") {
+    return;
+  }
+
+  const confirmAction = () => {
+    if (window.confirm(message)) {
+      callback();
+    } else {
+      rejection();
+    }
+  };
+  return confirmAction;
+};
+
+const App = () => {
+  const deleteWorld = () => console.log("Deleting the world...");
+  const abort = () => console.log("Aborted");
+  const confirmDelete = useConfirm("Are you sure", deleteWorld);
+  return (
+    <div className="App">
+      <button onClick={confirmDelete}>Delete the world</button>
+    </div>
+  );
+};
+
+
+export default App;
